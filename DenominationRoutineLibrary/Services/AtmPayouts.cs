e675563a85ee3efd802cd5b1cbb72c6d95ba8101
Leonardo Leme <leonardo.leme_wipro@akadseguros.com.br>
2024-03-13 20:38:09 -0300

    Added PossiblePayouts method implementation, that recieves an int with the value and returns a list of possible combinations

--- DenominationRoutineLibrary/Services/AtmPayouts.cs
+++ DenominationRoutineLibrary/Services/AtmPayouts.cs
@@ -9,6 +9,48 @@ public static class AtmPayouts
 
     public static List<List<BankNotesCombination>> PossiblePayouts(int payment)
     {
-        throw new NotImplementedException();
+        cartridges = cartridges.OrderByDescending(i => i).ToList();
+
+        var possibilities = GetPossibilities(payment, 0);
+        for (int i = 0; i < possibilities.Count; i++)
+        {
+            possibilities[i] = possibilities[i].OrderByDescending(i => i.BankNotesValue).ToList();
+        }
+
+        return possibilities;
+    }
+
+    private static List<List<BankNotesCombination>> GetPossibilities(int payment, int currentCartridge)
+    {
+        var possibilities = new List<List<BankNotesCombination>>();
+        var note = cartridges[currentCartridge];
+        var notePossibilities = payment / note;
+
+        if (cartridges.Count - 1 == currentCartridge)
+        {
+            possibilities.Add(new List<BankNotesCombination>() { new BankNotesCombination(notePossibilities, note)});
+            return possibilities;
+        }
+
+        for (int i = 0; i <= notePossibilities; i++)
+        {
+            var remaining = payment - (i * note);
+
+            if (remaining == 0) 
+            {
+                possibilities.Add(new List<BankNotesCombination>() { new BankNotesCombination(i, note) });
+                continue;
+            }
+
+            var insidePossibilities = GetPossibilities(remaining, currentCartridge + 1);
+            foreach (var possibility in insidePossibilities) 
+            {
+                if(i > 0)
+                    possibility.Add(new BankNotesCombination(i, note));
+                possibilities.Add(possibility);
+            }
+        }
+
+        return possibilities;
     }
 }
