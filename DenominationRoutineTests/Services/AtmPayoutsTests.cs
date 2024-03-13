c4ee290f4483c12698373fcd02176b1e0a4158fc
Leonardo Leme <leonardo.leme_wipro@akadseguros.com.br>
2024-03-13 14:32:26 -0300

    Made first test suit with more 5 possible inputs

--- DenominationRoutineTests/Services/AtmPayoutsTests.cs
+++ DenominationRoutineTests/Services/AtmPayoutsTests.cs
@@ -9,14 +9,161 @@ public class AtmPayoutsTests
     [TestMethod]
     public void PossiblePayouts_30()
     {
-        var expected = new List<List<BankNotesCombination>>();
-        var possible = new List<BankNotesCombination>
+        var expected = new List<List<BankNotesCombination>>() 
+        {
+            new List<BankNotesCombination>() 
             {
                 new BankNotesCombination(3, 10)
-            };
-        expected.Add(possible);
+            }
+        };
 
         var actual = AtmPayouts.PossiblePayouts(30);
+
+        AssertAtmPayouts(actual, expected);
+    }
+
+    [TestMethod]
+    public void PossiblePayouts_50()
+    {
+        var expected = new List<List<BankNotesCombination>>()
+        {
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(5, 10)
+            },
+            new List<BankNotesCombination>() 
+            {
+                new BankNotesCombination(1, 50)
+            }
+        };
+
+        var actual = AtmPayouts.PossiblePayouts(50);
+
+        AssertAtmPayouts(actual, expected);
+    }
+
+    [TestMethod]
+    public void PossiblePayouts_60()
+    {
+        var expected = new List<List<BankNotesCombination>>()
+        {
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(6, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 50),
+                new BankNotesCombination(1, 10)
+            }
+        };
+
+        var actual = AtmPayouts.PossiblePayouts(60);
+
+        AssertAtmPayouts(actual, expected);
+    }
+
+    [TestMethod]
+    public void PossiblePayouts_80()
+    {
+        var expected = new List<List<BankNotesCombination>>()
+        {
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(8, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 50),
+                new BankNotesCombination(3, 10)
+            }
+        };
+
+        var actual = AtmPayouts.PossiblePayouts(80);
+
+        AssertAtmPayouts(actual, expected);
+    }
+
+    [TestMethod]
+    public void PossiblePayouts_140()
+    {
+        var expected = new List<List<BankNotesCombination>>()
+        {
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(14, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 50),
+                new BankNotesCombination(9, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(2, 50),
+                new BankNotesCombination(4, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 100),
+                new BankNotesCombination(4, 10)
+            }
+        };
+
+        var actual = AtmPayouts.PossiblePayouts(140);
+
+        AssertAtmPayouts(actual, expected);
+    }
+
+    [TestMethod]
+    public void PossiblePayouts_230()
+    {
+        var expected = new List<List<BankNotesCombination>>()
+        {
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(23, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 50),
+                new BankNotesCombination(19, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(2, 50),
+                new BankNotesCombination(13, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(3, 50),
+                new BankNotesCombination(8, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(4, 50),
+                new BankNotesCombination(3, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 100),
+                new BankNotesCombination(13, 10)
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 100),
+                new BankNotesCombination(1, 50),
+                new BankNotesCombination(8, 10),
+            },
+            new List<BankNotesCombination>()
+            {
+                new BankNotesCombination(1, 100),
+                new BankNotesCombination(2, 50),
+                new BankNotesCombination(3, 10),
+            }
+        };
+
+        var actual = AtmPayouts.PossiblePayouts(230);
 
         AssertAtmPayouts(actual, expected);
     }
