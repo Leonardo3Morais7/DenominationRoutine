68bc121e0dff7f5627d144eab2cccd952bc80fa4
Leonardo Leme <leonardo.leme_wipro@akadseguros.com.br>
2024-03-13 14:17:20 -0300

    Added classLibrary with notes object and atm payouts service

---
+++ DenominationRoutineLibrary/Domain/BankNotesCombination.cs
@@ -0,0 +1,23 @@
+﻿namespace DenominationRoutineLibrary.Domain;
+
+public class BankNotesCombination
+{
+    public int BankNotesCount { get; set; }
+    public int BankNotesValue { get; set; }
+
+    public BankNotesCombination(int noteCount, int noteValue)
+    {
+        BankNotesCount = noteCount;
+        BankNotesValue = noteValue;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        var other = obj as BankNotesCombination;
+
+        return BankNotesCount == other.BankNotesCount && BankNotesValue == other.BankNotesValue;
+    }
+}
---
+++ DenominationRoutineLibrary/Services/AtmPayouts.cs
@@ -0,0 +1,14 @@
+﻿using DenominationRoutineLibrary.Domain;
+using System.Collections.Generic;
+
+namespace DenominationRoutineLibrary.Services;
+
+public static class AtmPayouts
+{
+    private static List<int> cartridges =  new List<int>() {10,50,100};
+
+    public static List<List<BankNotesCombination>> PossiblePayouts(int payment)
+    {
+        throw new NotImplementedException();
+    }
+}
